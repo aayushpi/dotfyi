6ecc9a3359ea86b44a2148f7e75fbe6289280319
Aayush Iyer <aayush.iyer@gmail.com>
2021-02-02 08:59:52 -0800

    docs(web): fix collapsed column

--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,26 @@
-import Head from 'next/head';
-import { StylingGlobals, styled, ThemeProvider, css } from '@twilio-paste/styling-library';
-import { pasteBaseStyles } from '@twilio-paste/theme';
-import { Box } from '@twilio-paste/core';
-import { PortfolioTheme } from '../theme';
-import { ComponentProvider } from '../components/component-provider';
+import Head from "next/head";
+import {
+  StylingGlobals,
+  styled,
+  ThemeProvider,
+  css,
+} from "@twilio-paste/styling-library";
+import { pasteBaseStyles } from "@twilio-paste/theme";
+import { Box } from "@twilio-paste/core";
+import { PortfolioTheme } from "../theme";
+import { ComponentProvider } from "../components/component-provider";
 import { SiteFooter } from "../components/footer";
 const StyledBase = styled(Box)(pasteBaseStyles);
 const globalStyles = (props) =>
   css({
     html: {
-      fontSize: '100%',
+      fontSize: "100%",
       height: `100%`,
-      backgroundColor: `colorBackgroundBrand`
+      backgroundColor: `colorBackgroundBrand`,
     },
     body: {
       margin: 0,
-      fontSize: 'fontSize30',
+      fontSize: "fontSize30",
     },
   })(props);
 const App = ({ Component, pageProps }) => (
@@ -33,23 +38,19 @@ const App = ({ Component, pageProps }) => (
     </Head>
     <ComponentProvider>
       <ThemeProvider theme={PortfolioTheme}>
-      <StylingGlobals styles={globalStyles({ theme: PortfolioTheme })} />
-        <StyledBase
-          as="main"
-        >
-        <Box
-        as="section"
-        marginRight={['space50', 'space50', 'auto']}
-        marginLeft={['space50', 'space50', 'auto']}
-        marginTop={['space50', 'space30', 'space200']}
-        maxWidth={"size200"}
-        >
-          <Component {...pageProps} />
-        </Box>  
+        <StylingGlobals styles={globalStyles({ theme: PortfolioTheme })} />
+        <StyledBase as="main">
+          <Box
+            as="section"
+            marginRight={["space50", "space50", "auto"]}
+            marginLeft={["space50", "space50", "auto"]}
+            marginTop={["space50", "space30", "space200"]}
+            maxWidth={"size200"}
+          >
+            <Component {...pageProps} />
+          </Box>
           <SiteFooter />
-
         </StyledBase>
-        
       </ThemeProvider>
     </ComponentProvider>
   </>
