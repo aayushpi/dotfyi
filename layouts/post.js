aae8605cb35b2d1cff63ac95e8124df075643d0c
Aayush Iyer <aayush.iyer@gmail.com>
2021-02-27 17:36:41 -0800

    feat(web): update next

--- layouts/post.js
+++
@@ -1,8 +0,0 @@
-import react from 'react';
-const Layout = (frontMatter) => ({ children: content }) => (
-  <div>
-    <h1>{frontMatter.title} - Posties</h1>
-    {content}
-  </div>
-);
-export default Layout;
