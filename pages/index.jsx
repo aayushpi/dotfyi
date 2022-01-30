be6d90cb48d4d7cc0af4c94f185017c0e14f34a8
Aayush Iyer <aayush.iyer@gmail.com>
2022-01-30 13:49:37 -0800

    feat(add pills)

--- pages/index.jsx
+++ pages/index.jsx
@@ -10,6 +10,7 @@ import { Paragraph } from '@twilio-paste/core/paragraph';
 import { Heading } from '@twilio-paste/core/heading';
 import { Grid, Column } from '@twilio-paste/core/grid';
 import {OrderedList, UnorderedList, ListItem} from '@twilio-paste/core/list';
+import {DisplayPillGroup, DisplayPill} from '@twilio-paste/core/display-pill-group';
 
 
 export default () => {
@@ -59,6 +60,11 @@ export default () => {
                 </Heading>
                 <UnorderedList>
                   <ListItem>
+                    <DisplayPillGroup aria-label="Post Type">
+                      <DisplayPill>
+                        Blog Post
+                      </DisplayPill>
+                    </DisplayPillGroup>
                     <Link href="/writing/scaling_support" passHref>
                       <Anchor>
                         Growing Pains: How We Scaled Our Design System Support
